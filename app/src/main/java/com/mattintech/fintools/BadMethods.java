cb58ad04e141ef8cf09180bd93120199be891303
Matt Hills <mattintech@gmail.com>
2020-03-15 15:49:47 -0400

    adding comments

--- app/src/main/java/com/mattintech/fintools/BadMethods.java
+++ app/src/main/java/com/mattintech/fintools/BadMethods.java
@@ -9,8 +9,8 @@ public class BadMethods {
 
     public void createAnr(Long waitTime) {
 
+        //cause a wait on the UI thread > 500ms - Result = Application Not responding report (ANR)
         Thread thread = new Thread();
-
         try {
             thread.sleep(waitTime);
         } catch (Exception e) {
@@ -19,11 +19,12 @@ public class BadMethods {
     }
 
     public int divByZero(int num) {
-
+        //Attempt to device a number by Zero
         return num/0;
     }
 
     public void npe() {
+        //reference a object not full initialized.
         Object obj = null;
         obj.hashCode();
 
